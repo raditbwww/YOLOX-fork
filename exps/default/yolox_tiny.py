dadf502305ec9bb2584c54beac53262621a2a5f0
raditbwww <99741179+raditbwww@users.noreply.github.com>
2024-12-23 19:37:09 +0700

    Update yolox_tiny.py

--- exps/default/yolox_tiny.py
+++ exps/default/yolox_tiny.py
@@ -12,9 +12,20 @@ class Exp(MyExp):
         super(Exp, self).__init__()
         self.depth = 0.33
         self.width = 0.375
-        self.input_size = (416, 416)
+        self.input_size = (640, 640)
         self.mosaic_scale = (0.5, 1.5)
         self.random_size = (10, 20)
-        self.test_size = (416, 416)
+        self.test_size = (640, 640)
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
         self.enable_mixup = False
+        
+        # Train config
+        self.max_epoch = 200
+
+        # Define yourself dataset path
+        self.data_dir = "datasets/custom"
+        self.train_ann = "instances_train2017.json"
+        self.val_ann = "instances_val2017.json"
+        self.test_ann = "instances_test2017.json"
+
+        self.num_classes = 2
